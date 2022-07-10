333834debec31018c7c0af45399109aae2c0ca78
imranrhem <imran.rhemtulla@gmail.com>
2022-07-10 16:23:43 -0400

    saved trip_data2 as an rds

--- cancelled_trips.R
+++ cancelled_trips.R
@@ -1,7 +1,7 @@
 # Cancelled Trips 
 
-# install.packages("dplyr")
-library("dplyr")
+install.packages("dplyr")
+load("dplyr")
 
 # Any trip with duration less than 2 minutes is likely a 'cancelled trip'. 
 # Find out the number of such trips, record the information for your report and 
@@ -13,4 +13,6 @@ trip_data_cancelled <- filter(trip_data, trip_data$duration < 120)
 # remove cancelled trips from data set 
 trip_data2 <- trip_data %>%
   filter(trip_data$duration >= 120)
+
+saveRDS(trip_data2, "trip_data2.rds")
 
