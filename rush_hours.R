a1e88df9f00d24b2255a731f89a8db1a8dcb620e
imranrhem <imran.rhemtulla@gmail.com>
2022-07-10 20:19:10 -0400

    determined the 10 most frequent starting and ending stations during rush hours on weekdays

--- rush_hours.R
+++ rush_hours.R
@@ -4,7 +4,7 @@ library("dplyr")
 library("lubridate")
 
 # Load the trip data with no outliers
-trips <- trip_no_outliers
+trips <- readRDS("trip_no_outliers.rds")
 
 # Convert the starting time to date format
 trips <- trips %>% mutate(start_date = mdy_hm(start_date))
@@ -12,38 +12,98 @@ trips <- trips %>% mutate(start_date = mdy_hm(start_date))
 # Identify which day each trip started on
 trips$weekday = weekdays(trips$start_date)
 
-par(mfrow = c(5,1))
+#### Determine rush hours through all weekdays
 
-# Determine rush hours for Mondays
-monday_trips <- trips[trips$weekday == "Monday",]
-class(monday_trips$start_date)
-monday_trips$hour = as.numeric(format(monday_trips$start_date, "%H"))
-hist(monday_trips$hour)
+# Build new dataframe with only weekdays
+weekday_trips <- trips[trips$weekday == "Monday" | trips$weekday == "Tuesday" | 
+                       trips$weekday == "Wednesday" | trips$weekday == "Thursday" |
+                       trips$weekday == "Friday", ]
 
-# Determine rush hours for Tuesdays
-tuesday_trips <- trips[trips$weekday == "Tuesday",]
-class(tuesday_trips$start_date)
-tuesday_trips$hour = as.numeric(format(tuesday_trips$start_date, "%H"))
-hist(tuesday_trips$hour)
+# Create new column that takes the "hour" the trip begin at
+weekday_trips$hour = as.numeric(format(weekday_trips$start_date, "%H"))
 
-# Determine rush hours for Wednesdays
-wednesday_trips <- trips[trips$weekday == "Wednesday",]
-class(wednesday_trips$start_date)
-wednesday_trips$hour = as.numeric(format(wednesday_trips$start_date, "%H"))
-hist(wednesday_trips$hour)
+# Plot hours on a histogram for visualization
+hist(weekday_trips$hour, main = "Numer of Trips Throughout Weekdays", xlab = "Time in Hours")
+  # Rush hours are 8:00 (8am)
+  # And 17:00 5(pm)
 
-# Determine rush hours for Thursdays
-thursday_trips <- trips[trips$weekday == "Thursday",]
-class(thursday_trips$start_date)
-thursday_trips$hour = as.numeric(format(thursday_trips$start_date, "%H"))
-hist(thursday_trips$hour)
+#### Determine the busiest 10 starting and ending stations during rush hours on weekdays
 
-# Determine rush hours for Friday
-Friday_trips <- trips[trips$weekday == "Friday",]
-class(Friday_trips$start_date)
-Friday_trips$hour = as.numeric(format(Friday_trips$start_date, "%H"))
-hist(Friday_trips$hour)
+# Create new dataframe with only the trips during the morning rush hour
+morning_rush <- weekday_trips[weekday_trips$hour == 8,]
 
+# Create a table containing the 10 busiest starting station in order
+morning_starting <- table(morning_rush$start_station_name)
+morning_starting <- sort(morning_starting, decreasing = T, na.rm = T)[1:10]
 
+# Plot the 10 busiest starting station in the morning rush hour
+barplot(morning_starting, 
+        main = "Ten Busiest Starting Stations During Morning Rush Hour",
+        ylab = "Number of Trips",
+        cex.lab = 1.3,
+        cex.axis = 1,
+        cex.names = 0.6,
+        las = 2)
 
+# Create a table containing the 10 busiest ending station in order
+morning_ending <- table(morning_rush$end_station_name)
+morning_ending <- sort(morning_ending, decreasing = T, na.rm = T)[1:10]
+
+# Plot the 10 busiest ending stations in the morning rush hour
+barplot(morning_ending, 
+        main = "Ten Busiest Ending Stations During Morning Rush Hour",
+        ylab = "Number of Trips",
+        cex.lab = 1.3,
+        cex.axis = 1.2,
+        cex.names = 1.2,
+        las = 2)
+
+# Create a new dataframe with only the trips during the evening rush hour
+evening_rush <- weekday_trips[weekday_trips$hour == 17,]
+
+# Create a table containing the 10 busiest starting station in order
+evening_starting <- table(evening_rush$start_station_name)
+evening_starting <- sort(evening_starting, decreasing = T, na.rm = T)[1:10]
+
+# Plot the 10 busiest starting station in the evening rush hour
+barplot(evening_starting, 
+        main = "Ten Busiest Starting Stations During Evening Rush Hour",
+        ylab = "Number of Trips",
+        cex.lab = 1.3,
+        cex.axis = 1.2,
+        cex.names = 1.2,
+        las = 2)
+
+# Create a table containing the 10 busiest ending station in order
+evening_ending <- table(evening_rush$end_station_name)
+evening_ending <- sort(evening_ending, decreasing = T, na.rm = T)[1:10]
+
+# Plot the 10 busiest ending stations in the evening rush hour
+barplot(evening_ending, 
+        main = "Ten Busiest Ending Stations During Evening Rush Hour",
+        ylab = "Number of Trips",
+        cex.lab = 1.3,
+        cex.axis = 1.2,
+        cex.names = 1.2,
+        las = 2)
+
+##### Determine the busiest 10 stations during weekends
+
+# Build new dataframe with only weekends
+weekend_trips <- trips[trips$weekday == "Saturday" | trips$weekday == "Sunday",]
+
+# Make a table including the 10 most frequent starting stations on weekends
+weekend_starting <- table(weekend_trips$start_station_name)
+weekend_starting <- sort(weekend_starting, decreasing = T, na.rm = T)[1:10]
+
+# Plot the 10 busiest starting stations during the weekend
+barplot(evening_starting, 
+        main = "Ten Busiest Starting Stations During Evening Rush Hour",
+        ylab = "Number of Trips",
+        cex.lab = 1.3,
+        cex.axis = 1.2,
+        cex.names = 1.2,
+        las = 2)
+
+# Plot the 10 busiest ending stations during the wekeend
 
