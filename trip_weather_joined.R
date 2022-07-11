90d296bdd8095103ac0fc79e0dc48c1c47fedf15
imranrhem <imran.rhemtulla@gmail.com>
2022-07-10 22:05:25 -0400

    created correlation plot

--- trip_weather_joined.R
+++ trip_weather_joined.R
@@ -12,6 +12,7 @@ weather <- weather %>% mutate(precipitation_inches = as.numeric(precipitation_in
 
 # Replace empty values with "No Event" in events
 weather$events[weather$events == ""] <- "No Event"
+weather$events[weather$events == "rain"] <- "Rain"
 
 # Convert zip_code to character 
 weather$zip_code <- as.character(weather$zip_code)
@@ -32,8 +33,21 @@ station <- read.csv("station.csv")
 #### Join weather and join tables
 
 # Join station to trips using name and start station to get cities
-ts_joined <- inner_join(station,trip, by = c("name" = "start_station_name")
+ts_joined <- inner_join(station,trip, by = c("name" = "start_station_name"))
 
 # Join weather to ts_joined by date and city (ensures there are only weather reports for the same day and location)                      
 wt_joined <- inner_join(ts_joined, weather, by = c("start_date" = "date", "city" = "city"))
+table(wt_joined$events)
+
+# Determine the number of trips for each event (skewed as there are naturally more events)
+wt_events <- wt_joined %>%
+  group_by(events) %>%
+  summarize(Trips = n())
+
+# Create a new data set only containing the numerical weather measurements
+wt_numerical <- dplyr::select(wt_joined,c(duration,max_temperature_f:cloud_cover))
+
+# Create correlation plot
+wt_corr <- cor(wt_numerical)
+corrplot(wt_corr)
 
